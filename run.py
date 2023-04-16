b76b7402c6ad6cb992d49e4b882258c6514a41e4
AssetMendy <asset.mendy@gmail.com>
2023-04-16 20:19:05 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from tabulate import tabulate
+
+# Define the scope of the API access
+SCOPE = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+# Define the credentials and authorize the API access
+CREDS  = ServiceAccountCredentials.from_json_keyfile_name('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT  = gspread.authorize(SCOPED_CREDS)
+
+# Open the Google Sheet
+SHEET = GSPREAD_CLIENT.open('my_movie_ratings').worksheet('movies')
